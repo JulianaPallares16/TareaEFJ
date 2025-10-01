418d0518b9380cce1aa21759afc3ca3df67a94f5
julianap <194113895+JulianaPallares16@users.noreply.github.com>
2025-10-01 11:48:11 -0500

    feat: :sparkles: Add Repositories

---
+++ Application/Regions/CreateRegion.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Application.Regions
+{
+    public class CreateRegion
+    {
+        
+    }
+}
--- Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd2f3440552a18ebf9355f0156321b3457ae7a6c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+821e93ea54d10d13901ad59379ede0f876723315")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
